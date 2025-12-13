1327463fecfe6dab21cdb6920becae1898e74a68
SickenedLeaf <ksdshaun@gmail.com>
2025-12-13 23:51:50 +0800

    yesssss

---
+++ shaun-recent/src/application/Campfire.java
@@ -0,0 +1,7 @@
+package application;
+
+public class Campfire extends Obstacle {
+    public Campfire() {
+        super(4, 0, 1, true);
+    }
+}
---
+++ shaun-recent/src/application/Spikes.java
@@ -0,0 +1,7 @@
+package application;
+
+public class Spikes extends Obstacle {
+    public Spikes() {
+        super(3, 2, 0, false);
+    }
+}
